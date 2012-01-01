c5aee6ac37e0fe1019a0f9d4733971b56bc023a2
Jeremy Stephens <viking@pillageandplunder.net>
2012-01-01 08:55:13 -0600

    Add port option for SSH store

--- lib/keyrack/store/ssh.rb
+++ lib/keyrack/store/ssh.rb
@@ -5,18 +5,25 @@ module Keyrack
         @host = options['host']
         @user = options['user']
         @path = options['path']
+        @port = options['port'] || 22
       end
 
       def read
         begin
-          Net::SCP.download!(@host, @user, @path)
+          result = nil
+          Net::SSH.start(@host, @user, :port => @port) do |ssh|
+            result = ssh.scp.download!(@path)
+          end
+          result
         rescue Net::SCP::Error
           nil
         end
       end
 
       def write(data)
-        Net::SCP.upload!(@host, @user, StringIO.new(data), @path)
+        Net::SSH.start(@host, @user, :port => @port) do |ssh|
+          ssh.scp.upload!(StringIO.new(data), @path)
+        end
       end
     end
   end
--- test/keyrack/store/test_ssh.rb
+++ test/keyrack/store/test_ssh.rb
@@ -4,23 +4,31 @@ module Keyrack
   module Store
     class TestSSH < Test::Unit::TestCase
       def test_read
-        store = SSH.new('host' => 'example.com', 'user' => 'dude', 'path' => 'foo.txt')
-        Net::SCP.expects(:download!).with("example.com", "dude", "foo.txt").returns("foo")
+        store = SSH.new('host' => 'example.com', 'user' => 'dude', 'port' => 22, 'path' => 'foo.txt')
+        scp = mock('scp session')
+        scp.expects(:download!).with("foo.txt").returns("foo")
+        session = mock('ssh session', :scp => scp)
+        Net::SSH.expects(:start).with('example.com', 'dude', :port => 22).yields(session)
         assert_equal "foo", store.read
       end
 
       def test_write
         store = SSH.new('host' => 'example.com', 'user' => 'dude', 'path' => 'foo.txt')
-        Net::SCP.expects(:upload!).with do |host, user, local, remote|
-          host == 'example.com' && user == 'dude' && local.is_a?(StringIO) &&
-            local.read == "foo" && remote == "foo.txt"
+        scp = mock('scp session')
+        session = mock('ssh session', :scp => scp)
+        Net::SSH.expects(:start).with('example.com', 'dude', :port => 22).yields(session)
+        scp.expects(:upload!).with do |local, remote|
+          local.is_a?(StringIO) && local.read == "foo" && remote == "foo.txt"
         end
         store.write("foo")
       end
 
       def test_read_returns_nil_for_non_existant_file
-        store = SSH.new('host' => 'example.com', 'user' => 'dude', 'path' => 'foo.txt')
-        Net::SCP.expects(:download!).with("example.com", "dude", "foo.txt").raises(Net::SCP::Error)
+        store = SSH.new('host' => 'example.com', 'user' => 'dude', 'port' => 22, 'path' => 'foo.txt')
+        scp = mock('scp session')
+        scp.expects(:download!).with("foo.txt").raises(Net::SCP::Error)
+        session = mock('ssh session', :scp => scp)
+        Net::SSH.expects(:start).with('example.com', 'dude', :port => 22).yields(session)
         assert_nil store.read
       end
     end

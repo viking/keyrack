51c1b6654d8dff8aec43e849f91572a752fd6a77
Jeremy Stephens <viking@pillageandplunder.net>
2012-12-17 14:18:23 -0600

    Remove unneeded methods from Utils

--- lib/keyrack/utils.rb
+++ lib/keyrack/utils.rb
@@ -7,23 +7,5 @@ module Keyrack
       end
       result
     end
-
-    def self.generate_rsa_key(password)
-      rsa = OpenSSL::PKey::RSA.new(4096)
-      cipher = OpenSSL::Cipher::Cipher.new('des3')
-      [rsa, rsa.to_pem(cipher, password)]
-    end
-
-    def self.generate_aes_key
-      SecureRandom.base64(128)[0..127]
-    end
-
-    def self.open_rsa_key(path, password)
-      OpenSSL::PKey::RSA.new(File.read(path), password)
-    end
-
-    def self.open_aes_data(path, rsa_key)
-      Marshal.load(rsa_key.private_decrypt(File.read(path)))
-    end
   end
 end
--- test/unit/test_utils.rb
+++ test/unit/test_utils.rb
@@ -5,35 +5,4 @@ class TestUtils < Test::Unit::TestCase
     result = Keyrack::Utils.generate_password
     assert_match /^[!-~]{8}$/, result
   end
-
-  def test_generate_rsa_key
-    rsa = mock('rsa')
-    OpenSSL::PKey::RSA.expects(:new).with(4096).returns(rsa)
-    cipher = mock('cipher')
-    OpenSSL::Cipher::Cipher.expects(:new).with('des3').returns(cipher)
-    rsa.expects(:to_pem).with(cipher, 'secret').returns('private key')
-
-    assert_equal([rsa, 'private key'], Keyrack::Utils.generate_rsa_key('secret'))
-  end
-
-  def test_generate_aes_key
-    SecureRandom.expects(:base64).with(128).returns("x" * 172)
-    result = Keyrack::Utils.generate_aes_key
-    assert_equal 128, result.length
-  end
-
-  def test_open_rsa_key
-    rsa_path = fixture_path('id_rsa')
-    rsa = mock('rsa')
-    OpenSSL::PKey::RSA.expects(:new).with(File.read(rsa_path), 'secret').returns(rsa)
-    assert_equal(rsa, Keyrack::Utils.open_rsa_key(rsa_path, 'secret'))
-  end
-
-  def test_open_aes_data
-    aes_path = fixture_path('aes')
-    aes = {'key' => '12345', 'iv' => '54321'}
-    rsa = mock('rsa')
-    rsa.expects(:private_decrypt).with(File.read(aes_path)).returns(Marshal.dump(aes))
-    assert_equal(aes, Keyrack::Utils.open_aes_data(aes_path, rsa))
-  end
 end
